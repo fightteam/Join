6f2bee8e0d771706c33d23a1c723ac266219406a
刘真源 <lzy7750015@qq.com>
2014-01-24 00:19:06 +0800

    add log sample

---
+++ samples/hello-log/src/main/java/org/fightteam/join/samples/log/User.java
@@ -0,0 +1,22 @@
+package org.fightteam.join.samples.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 简单测试类
+ * @author excalibur
+ * @since 0.0.1
+ */
+public class User {
+    private final static Logger log = LoggerFactory.getLogger(User.class);
+
+
+    public void info(){
+        log.info("info test");
+    }
+
+    public void debug(){
+        log.debug("debug test");
+    }
+}
