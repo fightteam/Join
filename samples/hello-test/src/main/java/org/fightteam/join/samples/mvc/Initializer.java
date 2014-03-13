9955a2b1578c8b71f7e16d775477608beaecfd5b
excalibur <excalibur-faith@qq.com>
2014-03-13 18:51:24 +0800

    add a sample hello test

---
+++ samples/hello-test/src/main/java/org/fightteam/join/samples/mvc/Initializer.java
@@ -0,0 +1,27 @@
+package org.fightteam.join.samples.mvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * [description]
+ *
+ * @author faith
+ * @since 0.0.1
+ */
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{MvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
