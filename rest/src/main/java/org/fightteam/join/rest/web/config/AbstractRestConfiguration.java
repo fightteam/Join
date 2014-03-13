427c085d6c7478e6926678cbea91afde9c224fb9
excalibur <excalibur-faith@qq.com>
2014-03-13 13:58:24 +0800

    change rest module config . now only a abstract class

--- rest/src/main/java/org/fightteam/join/rest/web/config/AbstractRestConfiguration.java
+++ rest/src/main/java/org/fightteam/join/rest/web/config/AbstractRestConfiguration.java
@@ -1,9 +1,6 @@
 package org.fightteam.join.rest.web.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.springframework.http.MediaType;
 
 /**
  * [description]
@@ -11,8 +8,7 @@ import org.springframework.http.MediaType;
  * @author faith
  * @since 0.0.1
  */
-@Configuration
-public class AbstractRestConfiguration extends RepositoryRestMvcConfiguration {
+public abstract class AbstractRestConfiguration extends RepositoryRestMvcConfiguration {
 
 //    @Override
 //    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
