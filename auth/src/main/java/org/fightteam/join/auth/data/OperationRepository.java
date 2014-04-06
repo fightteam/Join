fb5ed37c00cbf8315f2cae8f3fc4bc2827ba9518
刘真源 <excalibur-faith@qq.com>
2014-04-06 20:25:50 +0800

    remove jar

--- auth/src/main/java/org/fightteam/join/auth/data/OperationRepository.java
+++ auth/src/main/java/org/fightteam/join/auth/data/OperationRepository.java
@@ -3,14 +3,17 @@ package org.fightteam.join.auth.data;
 import org.fightteam.join.auth.data.models.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
- * [description]
+ * 操作查询
  *
  * @author faith
  * @since 0.0.1
  */
 public interface OperationRepository extends JpaRepository<Operation, Long> {
 
-    Operation findByTitle(String title);
+    Operation findByTitle(@Param("title")String title);
+
+    Operation findByName(@Param("name")String name);
 }
